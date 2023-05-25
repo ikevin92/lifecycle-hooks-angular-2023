9cb9e67012bcbbd1a7552b011de81e86f1259346
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-25 13:56:18 -0500

    finalizacion

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css'],
 })
-export class PriceComponent {}
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input()
+  public price: number = 0;
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('PriceComponent - ngOnInit');
+
+    this.interval$ = interval(1000).subscribe((value) =>
+      console.log('ticks:', { value })
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('PriceComponent - ngOnChanges');
+    console.log({ changes });
+  }
+
+  ngOnDestroy(): void {
+    console.log('PriceComponent - ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
+}
--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,8 +1,61 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css'],
 })
-export class ProductPageComponent {}
+export class ProductPageComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit()');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('ngOnChanges()');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck()');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked()');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit()');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked()');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy()');
+  }
+
+  increasePrice() {
+    this.currentPrice++;
+  }
+}
